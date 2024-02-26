c12fad4b0e786e09c26f095db338b7649468d9db
Andree Amaro <aamaro@skytouchtechnology.com>
2024-02-25 18:41:15 -0600

    Implement filtering by price range

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,34 @@ import { Box } from '@mui/material'
 const marks = [
   {
     value: 0,
-    label: '0°C'
+    label: '$0'
+  },
+  {
+    value: 10,
+    label: '$10'
   },
   {
     value: 20,
-    label: '20°C'
+    label: '$20'
   },
   {
     value: 40,
-    label: '40°C'
+    label: '$40'
   },
   {
     value: 100,
-    label: '100°C'
+    label: '$100'
   }
 ]
 
 function valuetext (value) {
-  return `${value}°C`
+  return `$${value}`
 }
 
 function App () {
   const [query, setQuery] = useState('')
   const { expenses, getExpenses } = useExpenses({ query })
+  const [value, setValue] = useState([0, 100])
 
   const handleChange = (event) => {
     const newQuery = event.target.value
@@ -38,9 +43,23 @@ function App () {
     getExpenses({ search: newQuery })
   }
 
+  const minDistance = 10
+
+  const handleSliderChange = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return
+    }
+
+    if (activeThumb === 0) {
+      setValue([Math.min(newValue[0], value[1] - minDistance), value[1]])
+    } else {
+      setValue([value[0], Math.max(newValue[1], value[0] + minDistance)])
+    }
+    getExpenses({ query, range: newValue })
+  }
+
   function handleSubmit (event) {
     event.preventDefault()
-    // getExpenses({ query })
   }
 
   return (
@@ -58,7 +77,9 @@ function App () {
       <Box
         sx={{
           width: '50%',
-          display: 'flex'
+          display: 'flex',
+          justifyContent: 'center',
+          marginLeft: '25px'
         }}
       >
         <Slider
@@ -67,11 +88,14 @@ function App () {
               color: '#bcbfcc'
             }
           }}
-          aria-label='Restricted values'
+          value={value}
+          getAriaLabel={() => 'Temperature range'}
           getAriaValueText={valuetext}
+          onChange={handleSliderChange}
           step={10}
           marks={marks}
           valueLabelDisplay='auto'
+          disableSwap
         />
       </Box>
       <main>
--- src/hooks/useExpenses.js
+++ src/hooks/useExpenses.js
@@ -1,18 +1,24 @@
 import { RESULTS } from '../mocks/withResults.js'
 import { useState } from 'react'
 
-export function useExpenses ({ query }) {
+export function useExpenses ({ query, range }) {
   const [expenses, setExpenses] = useState({})
   const sortedResults = RESULTS.sort((a, b) => {
     return new Date(b.data.date) - new Date(a.data.date)
   })
-  const getExpenses = ({ search }) => {
+  const getExpenses = ({ search, range }) => {
     const newExpenses = sortedResults.filter((expense) => {
-      return (
-        expense.data.description.includes(search.toLowerCase()) ||
-        expense.data.accountName.toLowerCase().includes(search.toLowerCase()) ||
-        expense.data.date.toLowerCase().includes(search.toLowerCase())
-      )
+      if (search) {
+        return (
+          expense.data.description.includes(search.toLowerCase()) ||
+          expense.data.accountName.toLowerCase().includes(search.toLowerCase()) ||
+          expense.data.date.toLowerCase().includes(search.toLowerCase()) ||
+          (
+            expense.data.amount >= range[0] && expense.data.amount <= range[1]
+          )
+        )
+      }
+      return expense.data.amount >= range[0] && expense.data.amount <= range[1]
     })
     const groupedExpenses = Object.groupBy(newExpenses, ({ data }) => data.date)
     setExpenses(groupedExpenses)
